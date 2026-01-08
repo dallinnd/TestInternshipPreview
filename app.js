a5932165614f1051a54214299625cde4df2f60ce
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-01-08 13:21:28 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const internshipSources = [
+    {
+        name: "Pitt CSC List",
+        type: "markdown",
+        // Note the 'raw.githubusercontent.com' domain!
+        url: "https://raw.githubusercontent.com/pittcsc/Summer2024-Internships/dev/README.md" 
+    },
+    {
+        name: "Simplify Jobs",
+        type: "json", // or csv
+        url: "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/.github/scripts/listings.json"
+    }
+];
